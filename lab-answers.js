afd22b60de55467069947c53c3421335c394655c
Israajddn <Israajddn@gmail.com>
2023-10-14 11:14:42 -0400

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -24,3 +24,30 @@ for (let i= 1; i <= 100; i++){
   }
 }
 console.log(fb);
+
+//Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//1
+for (let i= 0; i < plantee.length; i++) {
+	if (typeof plantee[i] === "number") {
+        plantee[i] += 1;
+  }
+}
+//2
+for (let i= 0; i < wolfy.length; i++) {
+	if (wolfy[i] === "Yukon Territory") {
+        wolfy[i] = "Gotham City";
+  }
+}
+//3
+dart.push("Hawkins");
+//4
+wolfy.shift();
+wolfy.unshift("Gameboy");
+console.log(plantee);
+console.log(wolfy);
+console.log(dart);
