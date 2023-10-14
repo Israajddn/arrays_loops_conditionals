b3b7f171fed993ca14a2abdccec4da5cabbeb9d0
Israajddn <Israajddn@gmail.com>
2023-10-14 15:00:41 -0400

    Kristyn's lost shoe found

--- lab-answers.js
+++ lab-answers.js
@@ -132,9 +132,46 @@
 // }
 // ////////////////////////////////
 
-// Find the Median
-const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
-nums.sort();
-const median = Math.floor(nums.length / 2);;
-console.log(nums[median]);
+// // Find the Median
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+// nums.sort();
+// const median = Math.floor(nums.length / 2);;
+// console.log(nums[median]);
+// ////////////////////////////////
+
+// Hungry for More?
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "Per Scholas hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+  // Alien Attire
+  const kristynsShoe = kristynsCloset.shift();
+  thomsCloset[2].push(kristynsShoe);
+  console.log(thomsCloset[2]);
 ////////////////////////////////
