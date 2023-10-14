b4288bf793308c8c428bef30135129d5f38fd9b9
Israajddn <Israajddn@gmail.com>
2023-10-14 11:03:48 -0400

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -9,3 +9,18 @@ for (let i = 0; i <= 200; i++) {
         console.log(i);
     }
 }
+
+// Fizz Buzz
+const fb = [];
+for (let i= 1; i <= 100; i++){
+  if (i % 3 === 0 && i % 5 === 0) {
+    fb.push("fizzbuzz");
+  } else if (i % 3 === 0) {
+    fb.push("fizz");
+  } else if (i % 5 === 0) {
+    fb.push("buzz");
+  } else {
+    fb.push(i);
+  }
+}
+console.log(fb);
